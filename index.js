f650b163fec4760232bb50d82b4ec211683d8ef0
Ruian Duan <duanruian@gmail.com>
2017-03-31 16:35:30 -0400

    fixed the inter-gradle file dependency of ext variables.

--- index.js
+++ index.js
@@ -3,15 +3,17 @@
 
 var fs = require('fs');
 var parser = require('./lib/parser');
-var path = process.argv.slice(2).join('');
+var args = process.argv.slice(2);
+var path = args[0];
+var rootPaths = args.length > 1? args.slice(1): null;
 
 if (!path) {
-  console.error('No input detected');
+  console.error('No input detected! Usage: node index.js path [rootPath1 rootPath2 ...]');
   return;
 }
 
 if (fs.statSync(path)) {
-  parser.parseFile(path).then(function(parsedValue) {
+  parser.parseFile(path, rootPaths).then(function(parsedValue) {
     console.log(JSON.stringify(parsedValue, '', 2));
   });
 }
